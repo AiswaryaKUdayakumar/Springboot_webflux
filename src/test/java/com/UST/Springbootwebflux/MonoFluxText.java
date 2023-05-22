0660ce290c107ea7c2e45a2d09396b19251e7cbd
AiswaryaKUdayakumar <aiswaryakuday@gmail.com>
2023-05-22 23:48:35 +0530

    22-05-2023

---
+++ src/test/java/com/UST/Springbootwebflux/MonoFluxText.java
@@ -0,0 +1,26 @@
+package com.UST.Springbootwebflux;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class MonoFluxText {
+    @Test
+    public void testMono(){
+        Mono<?> monoString = Mono.just("javatechie")
+                .then(Mono.error(new RuntimeException("Exception occured")))
+                .log();
+        monoString.subscribe(System.out::println,(e)->System.out.println(e.getMessage()));
+    }
+    @Test
+    public void testFlux(){
+        Flux<String> fluxString = Flux.just("Spring", "Spring Boot", "Hibernate", "microservice")
+                .concatWithValues("AWS")
+                .concatWith(Flux.error(new RuntimeException("Exception occured in Flux")))
+                .concatWithValues("cloud")
+                .log();
+
+        fluxString.subscribe(System.out::println,(e)->System.out.println(e.getMessage()));
+
+    }
+}
